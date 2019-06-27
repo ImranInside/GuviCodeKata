6eb3cb9728c52f34b2f3be03a4a13a0b0f888cd1
ImranInside <52240789+ImranInside@users.noreply.github.com>
2019-06-27 10:26:06 +0530

    Update EvenOrOdd.java

--- EvenOrOdd.java
+++ EvenOrOdd.java
@@ -2,14 +2,17 @@ import java.Util.*;
 class EvenOrOdd{
   public static void main(String args[]){
     int number;
-    Scanner scrn = new Scanner;
-    number = scrn.nextInt();
-    if(number % 2 == 0){
-      System.out.print("The number is Even");
-    }
-    else{
-      System.out.print("The number is Odd");
-    }
+	    Scanner scrn = new Scanner(System.in);
+	    number = scrn.nextInt();
+	    if(number % 2 == 0){
+	      System.out.print("The number is Even");
+	    }
+	    else if (number % 2 > 0){
+	      System.out.print("The number is Odd");
+	    }
+	    else{
+	      System.out.print("The number is Invalid");
+	    }
   }
 }
   
